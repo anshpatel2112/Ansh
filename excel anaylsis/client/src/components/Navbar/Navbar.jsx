a60c88062ea674dddf94619e6d8406466db57366
Ansh Patel <anshpatel7623@gmail.com>
2025-08-01 19:53:21 +0530

    navbar

---
+++ excel anaylsis/client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+// This component is no longer used with the new layout
+// Keeping for backward compatibility
+import React from 'react';
+
+const Navbar = () => {
+  return null;
+};
+
+export default Navbar;
